d3ffdcec5db1bd56aea79a0b115ed6979d203423
tsemayess <80348447+tsemayess@users.noreply.github.com>
2021-05-11 07:09:28 -0400

    Added main service classes

---
+++ src/services/UserService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const USER_API_BASE_URL = 'http://localhost:8080/users';
+
+class UserService {
+
+    register(user){
+        return axios.post(USER_API_BASE_URL + '/user', user)
+    }
+
+}
+
+export default new UserService();
