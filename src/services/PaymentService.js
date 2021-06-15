099d9b0e0e3a6ca697d8e46afadf28fdacd418ce
tsemayess <tsemaye.oseragbaje@smoothstack.com>
2021-06-15 10:55:32 -0400

    Added Jenkinsfile needs work and deleted images directory. Additionally, payment service now pulls token from local storage

--- src/services/PaymentService.js
+++ src/services/PaymentService.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 const PAYMENT_SERVICE_BASE_URL = 'http://localhost:8043/payment';
-const jwtToken = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5NiIsImV4cCI6MTYyMzc3ODU3MywiaWF0IjoxNjIzNDE4NTczfQ.v939q-BUHDRxg8LWXm15DCFQCcnS2d8rDaK_gpI9GEI';
-//in final pull jwt from local storage
+const jwtToken = 'Bearer ' + localStorage.getItem('jwt');
 const config = {
     headers: {
         'Content-Type': 'application/json',
